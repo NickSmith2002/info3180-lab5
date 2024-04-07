536fd4b697cf021bbdd1d79c9a364f0d34f09dfe
Nicholas Smith <nicholaswsmith2002@gmail.com>
2024-04-07 13:46:42 -0500

    Exercise 3 Completed

--- app/views.py
+++ app/views.py
@@ -5,10 +5,13 @@ Werkzeug Documentation:  https://werkzeug.palletsprojects.com/
 This file creates your application.
 """
 
-from app import app
-from flask import render_template, request, jsonify, send_file
+from app import app, db
+from werkzeug.utils import secure_filename
+from app.models import Movie
+from app.forms import MovieForm
+from flask import render_template, request, jsonify, send_file, send_from_directory
+from flask_wtf.csrf import generate_csrf
 import os
-
 
 ###
 # Routing for your application.
@@ -18,6 +21,49 @@ import os
 def index():
     return jsonify(message="This is the beginning of our API")
 
+@app.route('api/v1/movies', methods=['POST'])
+def movies():
+    form = MovieForm()
+
+    if form.validate_on_submit():
+        title = form.title.data
+        description = form.description.data
+        poster = request.files.get('poster')
+
+        filename = secure_filename(poster.filename)
+        UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
+        if not os.path.exists(UPLOAD_FOLDER):
+            os.makedirs(UPLOAD_FOLDER)
+        poster.save(os.path.join(UPLOAD_FOLDER, filename))
+
+        movie = Movie(title=title, description=description, poster=filename if poster else None)
+
+        db.session.add(movie)
+        db.session.commit()
+
+        #Success message is returned to users if the movie is added
+        response = {
+            'message': 'Movie Successfully added',
+            'title': movie.title,
+            'poster': movie.poster,
+            'description': movie.description
+        }
+        return jsonify(response), 201
+
+    errors = form_errors(form)
+    return jsonify(errors=errors), 400
+
+@app.route('/api/v1/movielist', methods=['GET'])
+def get_movies():
+    movies = Movie.query.all()
+    return jsonify({'movies': [{'id': movie.id,
+                                'title': movie.title,
+                                'description': movie.description,
+                                'poster': f"/api/v1/posters/{movie.poster}"} for movie in movies]})
+
+@app.route('/api/v1/posters/<filename>', methods=['GET'])
+def get_poster(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 ###
 # The functions below should be applicable to all Flask apps.
