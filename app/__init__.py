01382b0275626c372712406302ebf1fbd8402183
Nicholas Smith <nicholaswsmith2002@gmail.com>
2024-04-07 13:13:57 -0500

    Exercise 2 Completed

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,18 @@
+import os
 from flask import Flask
 from .config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
 
+UPLOAD_FOLDER = "uploads"
 app = Flask(__name__)
-app.config.from_object(Config)
 
+app.config.from_object(Config)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+cref = CSRFProtect(app)
 from app import views
+
--- app/models.py
+++ app/models.py
@@ -1,1 +1,20 @@
 # Add any model classes for Flask-SQLAlchemy here
+from . import db
+import datetime
+
+class Movie(db.Model):
+
+    __tablename__ = 'movies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128))
+    description = db.Column(db.Text)
+    string = db.Column(db.String(128))
+    poster = db.Column(db.String(255)) #Accounts for Large Poster Urls
+    created_at = db.Column(db.DateTime, default=datetime.now())
+
+    def __init__(self, title, description, poster):
+        self.title = title
+        self.description = description
+        self.poster = poster
+        self.created_at = datetime.now()
